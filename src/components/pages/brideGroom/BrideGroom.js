fc3c4b64c216d2b223c632ba056e38a3cce1d8f9
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-09-16 23:59:59 +0200

    modify names

--- src/components/pages/brideGroom/BrideGroom.js
+++ src/components/pages/brideGroom/BrideGroom.js
@@ -13,12 +13,12 @@ export default function BrideGroom() {
       <div className="flex flex-col md:flex-row justify-center items-center py-20 md:gap-16 gap-8 px-2">
         <div className="flex flex-col justify-center items-center gap-5">
           <img src={foto1} className="w-56 md:w-72" alt="foto sposo" />
-          <p>Matteo S.</p>
+          <p>Matteo</p>
         </div>
         <p className="text-4xl sm:text-6xl lg:text-6xl chopin">&</p>
         <div className="flex flex-col justify-center items-center gap-5">
           <img src={foto2} className="w-56 md:w-72" alt="foto sposa" />
-          <p>Alessandra L.</p>
+          <p>Alessandra</p>
         </div>
       </div>
       <div className="text-center chopin text-5xl">Lorem, ipsum dolor. </div>
